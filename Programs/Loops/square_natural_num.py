edd0f727b04ce9159d7ed396d8b4ae148885368d
Kushagra mathuria <kushagramathuria9@gmail.com>
2026-02-08 22:51:05 +0530

    Add files via upload

---
+++ Programs/Loops/square_natural_num.py
@@ -0,0 +1,9 @@
+# square of all natural num 1 to 10 using for loop
+for i in range(1, 11):
+    print(i*i, end=" ")
+
+# square of all natural num 1 to 10 using while loop
+i= 1
+while i<=10:
+    print(i*i, end=" ")
+    i+=1
---
+++ Programs/Loops/tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+n = int(input())
+# temp= n
+# sum = 0
+# while n>0:
+#     d = n%10
+#     sum=sum+(d*d*d)
+#     n = n//10
+# if temp == sum:
+#     print("armstrong")
+# else:
+#     print("not armstrong")
