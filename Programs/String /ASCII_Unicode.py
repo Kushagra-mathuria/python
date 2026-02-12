df6bb70b04374ba2cd813dee5839d542a04c7a68
Kushagra mathuria <kushagramathuria9@gmail.com>
2026-02-12 13:31:59 +0530

    Delete Programs/String  directory

--- Programs/String /ASCII_Unicode.py
+++
@@ -1,8 +0,0 @@
-# ASCII: it uses no. 0 to 127 to represent characters.
-print(ord("A"))
-print(ord("a"))
-
-# unicode: it supports characters from all languages + emojis.
-# python uses unicode, so it supports every character. 
-print(chr(65))
-print(chr(5678))
