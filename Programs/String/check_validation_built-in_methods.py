a11073f43cf7c9e144acb40a8c506fb806f415ae
Kushagra mathuria <kushagramathuria9@gmail.com>
2026-02-16 14:21:57 +0530

    Replace string examples with new variable values

--- Programs/String/check_validation_built-in_methods.py
+++ Programs/String/check_validation_built-in_methods.py
@@ -1,35 +1,36 @@
 # isalnum(): returns True if all characters in the string are alphanumeric.
-a = "Avani123"
+a = "kushagra1234"
 print(a.isalnum())
 
 # isalpha(): returns True if all characters in the string are alphabet.
-a = "Avani123"
+a = "kushagra1234"
 print(a.isalpha())
 
 # isdigit(): returns True if all characters in the string are numeric (0 to 9).
-a = "Avani123"
+a = "kushagra1234"
 print(a.isdigit())
 
 # islower(): returns True if all characters in the string are lowercase.
-a = "Avani123"
+a = "kushagra1234"
 print(a.islower())
 
 # isupper(): returns True if all characters in the string are uppercase.
-a = "Avani123"
+a = "kushagra1234"
 print(a.isupper())
 
 # istitle(): returns True if the string follows the rules of title.
-a = "Avani123"
+a = "kushagra1234"
 print(a.istitle())
 
 # isspace(): returns True if all characters in the string are whitespace.
-a = "Avani123"
+a = "kushagra1234"
 print(a.isspace())
 
 # startswith(): returns True if the string starts with the specified value.
-a = "Avani123"
+a = "kushagra1234"
 print(a.startswith("A"))
 
 # endswith(): returns True if the string ends with the specified value.
-a = "Avani123"
+a = "kushagra1234"
+
 print(a.endswith("3"))
