09a4353137321054b9febdb9774446ba47ff27d7
Kushagra mathuria <kushagramathuria9@gmail.com>
2026-02-17 00:42:21 +0530

    Update stored password in password.py

--- Programs/Conditional_statements/password.py
+++ Programs/Conditional_statements/password.py
@@ -1,8 +1,9 @@
 # Accept a password from the user; if it matches the stored one, print “Login Successful”, else “Try Again”.
 
 password = input("enter password: ")
-stored = "Avani_Sharma"
+stored = "kushagra_mathuria"
 if stored == password:
     print("login successful")
 else:
+
     print("try again")
