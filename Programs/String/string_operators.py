6e5a2a4aee18e2429b513c599e0d08ca8c2fa56f
Kushagra mathuria <kushagramathuria9@gmail.com>
2026-02-16 14:32:58 +0530

    Update string_operators.py

--- Programs/String/string_operators.py
+++ Programs/String/string_operators.py
@@ -1,16 +1,17 @@
 # concatenation (+): to add the strings with (+) operator.
-a = "Avani"
-b = "Sharma"
+a = "kushagra"
+b = "mathuria"
 c = a+b
 print(c)
 d = a+ " " +b
 print(d)
 
 # repeating string (*): repeat a string multiple times using * operator.
-a = "Avani"
+a = "kushagra"
 print(a*3)
 
 # string iteration (Membership operator):
 s = "Python"
 for char in s:
+
     print(char)
